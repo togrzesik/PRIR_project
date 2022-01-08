bd37cbde72da904c858cefbdcfc2b11ac154d13e
togrzesik <tomasz_grzesik.98@outlook.com>
2022-01-08 18:57:54 +0100

    before last time

--- MPI1/MPI1.cpp
+++ MPI1/MPI1.cpp
@@ -1,127 +1,100 @@
-﻿// PRIR_project.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <mpi.h>
+#include <cstdlib>
+#include <stdio.h>
+#include <string.h>
 
-#include <mpi.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-
-
-
-#define n 5 
-
-int a[] = { 2, 4, 7, 6, 5 };
+#define FILL_MODE_NOT_RANDOM 0
+#define FILL_MODE_RANDOM 1
 
 
-int a2[1000];
+int initVector(double* vector, unsigned int vSize, unsigned int fillMode, unsigned int procIndex) {
 
-int main(int argc, char* argv[])
-{
-
-	int pid, np,
-		elements_per_process,
-		n_elements_recieved;
-
-	MPI_Status status;
-
-
-	MPI_Init(&argc, &argv);
-
-
-	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
-	MPI_Comm_size(MPI_COMM_WORLD, &np);
-	double startParallel, stopParallel;
-	startParallel = MPI_Wtime();
-
-	if (pid == 0) {
-		int index, i;
-		elements_per_process = n / np;
+    for (unsigned int i = 0; i < vSize; i++) {
+        if (fillMode == FILL_MODE_RANDOM) {
+            vector[i] = rand() / (double)RAND_MAX;
+        }
+        else {
+            vector[i] = procIndex;
+        }
+    }
+    return 0;
+}
 
 
-		if (np > 1) {
+int main(int argc, char** argv) {
 
-			for (i = 1; i < np - 1; i++) {
-				index = i * elements_per_process;
+    MPI_Init(&argc, &argv); 
 
-				MPI_Send(&elements_per_process,
-					1, MPI_INT, i, 0,
-					MPI_COMM_WORLD);
-				MPI_Send(&a[index],
-					elements_per_process,
-					MPI_INT, i, 0,
-					MPI_COMM_WORLD);
-			}
+    unsigned int vSize = 2000;
+    if (argc == 2) { 
+        vSize = atoi(argv[1]);
+    }
 
+    unsigned int fillMode = FILL_MODE_NOT_RANDOM;
+    if (argc == 3) {
+        if (strcmp("-random", argv[3])) {
+            fillMode = FILL_MODE_RANDOM;
+        }
+    }
 
-			index = i * elements_per_process;
-			int elements_left = n - index;
-
-			MPI_Send(&elements_left,
-				1, MPI_INT,
-				i, 0,
-				MPI_COMM_WORLD);
-			MPI_Send(&a[index],
-				elements_left,
-				MPI_INT, i, 0,
-				MPI_COMM_WORLD);
-		}
+    int procCount, procIndex; 
 
 
-		int largest = INT_MIN;
-		for (i = 0; i < elements_per_process; i++)
-		{
-			if (a[i] > largest) {
-				largest = a[i];
-			}
-
-
-		}
+    MPI_Comm_size(MPI_COMM_WORLD, &procCount); 
+    MPI_Comm_rank(MPI_COMM_WORLD, &procIndex); 
 
 
 
-		int tmp;
-		for (i = 1; i < np; i++) {
-			MPI_Recv(&tmp, 1, MPI_INT,
-				MPI_ANY_SOURCE, 0,
-				MPI_COMM_WORLD,
-				&status);
-			int sender = status.MPI_SOURCE;
-			if (tmp > largest)
-				largest = tmp;
-		}
+    double startParallel, stopParallel; 
+    startParallel = MPI_Wtime();
 
 
-		printf("Largest is : %d\n", largest);
-	}
+    double* dataVector; 
+    double procMax;
+    double calculatedMax;
+    double realMax;
 
-	else {
-		MPI_Recv(&n_elements_recieved,
-			1, MPI_INT, 0, 0,
-			MPI_COMM_WORLD,
-			&status);
-
-
-		MPI_Recv(&a2, n_elements_recieved,
-			MPI_INT, 0, 0,
-			MPI_COMM_WORLD,
-			&status);
+    dataVector = (double*)malloc(vSize * sizeof(double));
+    realMax = initVector(dataVector, vSize, fillMode, procIndex);
 
 
-		int partial_largest = INT_MIN;
-		for (int i = 0; i < n_elements_recieved; i++)
-		{
-			if (a2[i] > partial_largest) {
-				partial_largest = a2[i];
-			}
-		}
-
-		MPI_Send(&partial_largest, 1, MPI_INT,
-			0, 0, MPI_COMM_WORLD);
-	}
-	stopParallel = MPI_Wtime();
-	printf("Execution time = %f", stopParallel - startParallel);
+    if (procIndex == 0) {
+        printf("Array size per process is %u\n", vSize);
+    }
 
 
-	MPI_Finalize();
+    procMax = dataVector[0];
+    for (unsigned int i = 0; i < vSize; i++) {
+        if (dataVector[i] > procMax) {
+            procMax = dataVector[i];
+        }
+    }
 
-	return 0;
+    MPI_Reduce(&procMax, &calculatedMax, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    if (procIndex == 0) {
+        if (fillMode == FILL_MODE_NOT_RANDOM) {
+            if (calculatedMax == procCount - 1) {
+                printf("Max parallel = %f\n", calculatedMax);
+            }
+            else {
+                printf("No Success Calculation Maximum! You must recalculate this!");
+                return 1;
+            }
+        }
+    }
+
+
+    stopParallel = MPI_Wtime();
+    if (procIndex == 0) {
+        printf("Execution time= %f, process count= %d, size= %u\n",
+            stopParallel - startParallel,
+            procCount,
+            vSize
+        );
+    }
+
+    MPI_Finalize();
+
+    return 0;
 }
