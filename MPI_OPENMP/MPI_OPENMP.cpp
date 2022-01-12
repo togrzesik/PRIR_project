3a6dcfda0868cf686ebd5fbbc7e821797b1904b5
togrzesik <tomasz_grzesik.98@outlook.com>
2022-01-12 22:43:53 +0100

    better version

--- MPI_OPENMP/MPI_OPENMP.cpp
+++ MPI_OPENMP/MPI_OPENMP.cpp
@@ -4,12 +4,14 @@
 #include <queue>
 #include <mpi.h>
 #include <omp.h>
+
 using namespace std;
 
 /*int KthLargest1(vector<int>& nums, int k)
 {
     int n = nums.size();
     sort(nums.begin(), nums.end());
+
     return nums[n - k];
 } */
 
@@ -33,31 +35,69 @@ int KthLargest2(vector<int>& nums, int k)
 
     return pq.top();
 }
-
+#define MAX 50000
 int main(int argc, char* argv[])
 {
     vector<int> nums;
 
+    float tab[MAX];
     int k = 3;
-    int th = 2;
     int my_rank;
-    int silk = 2000;
-    MPI_Status status;
+    int size = 10;
+    int i, d;
+    int y, z;
+    int wartosc;
+    int reszta;
+    double tmp = 0;
+    double wynik = 0;
+    double startParallel = 0.0, stopParallel = 0.0;
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &th);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    double startParallel, stopParallel;
-    startParallel = MPI_Wtime();
-    #pragma omp parallel for num_threads(th)
-    for (int i(0); i < silk; i++)
-        nums.push_back(rand() % 10000);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Request reql;
+    MPI_Status status;
+    int* tabAll = new int{ size };
+    if (my_rank == 0) {
+        startParallel = MPI_Wtime();
+        for (d = 0; d < size; d++) {
+            if (d != my_rank) {
+                int a = 4;
+                MPI_Send(&a, 1, MPI_INT, d, 13, MPI_COMM_WORLD);
+            }
+        }
+    }
+    else if (my_rank != 0) {
+        int b;
+        MPI_Recv(&b, 1, MPI_INT, 0, 13, MPI_COMM_WORLD, &status);
+    }
+    reszta = MAX % size;
+    if (reszta == 0) wartosc = MAX / size;
+    else {
+        if (my_rank == 0) wartosc = MAX;
+        else wartosc = 0;
+        if (my_rank == 0) {
+            cout << "Nie można podzielić tablicy na %d " << size << "równych części!";
+            cout << "K-ta wartość zostanie wyliczona przez 1 proces.";
+        }
+    }
+    y = wartosc * my_rank;
+    z = wartosc * my_rank + wartosc;
+    for (i = y; i < z; i++) {
+        tab[i] = i + 1;
+    }
+#pragma omp parallel for num_threads(size)
+    for (int i = y; i < z; i++)
+        nums.push_back(rand() % 100000);
 
-//    MPI_Send(&silk, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-//    MPI_Recv(&k, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-    cout << "Array:";
-    cout << k << " Largest2:" << KthLargest2(nums, k) << endl;
     stopParallel = MPI_Wtime();
+    cout << "Array: ";
     double Diff = stopParallel - startParallel;
+    // for (int i : nums)
+      //   cout << i << "\n";
+
+    //cout << k << " Largest1:" << KthLargest1(nums, k) << endl;
+
+    cout << k << " Largest:" << KthLargest2(nums, k) << endl;
     cout << "Execution time: " << Diff;
     MPI_Finalize();
 }
