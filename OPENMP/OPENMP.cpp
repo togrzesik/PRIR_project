e6d447d736a9df34b4a47b803ceac97556f35096
togrzesik <tomasz_grzesik.98@outlook.com>
2022-01-14 00:11:25 +0100

    better version

--- OPENMP/OPENMP.cpp
+++ OPENMP/OPENMP.cpp
@@ -33,26 +33,27 @@ int KthLargest2(vector<int>& nums, int k)
     }
 
     return pq.top();
+    
 }
 
 int main(int argc, char* argv[])
 {
     vector<int> nums;
-    unsigned long sum = 0;
-    int quantity = 0;
     int k = 3;
-    int th = 1;
-    int my_rank;
-    int silk = 5000;
-    double startParallel, stopParallel;
+    int th = 10;
+    omp_set_num_threads(th);
+    int n = 50000;
+    int i;
+    for (int i = 0; i < n; i++)
+        nums.push_back(rand() % 100000);
+    double startParallel = 0.0, stopParallel = 0.0;
     startParallel = omp_get_wtime();
-#pragma omp parallel for reduction(+:sum,quantity) num_threads(th)
-    for (int i(0); i < silk; i++)
-        nums.push_back(rand() % 10000);
-
-    cout << "Array:";
-    cout << k << " Largest2:" << KthLargest2(nums, k) << endl;
+#pragma omp parallel for num_threads(th)
+    for (int i = 0; i < th; i++)
+        KthLargest2(nums, k);
     stopParallel = omp_get_wtime();
+    cout << "Array: ";
+    cout << k << " Largest:" << KthLargest2(nums, k) << endl;
     double Diff = stopParallel - startParallel;
     cout << "Execution time: " << Diff;
 }
