ed97d83c62a42eaa83862e867d657b0f2bfa207e
togrzesik <tomasz_grzesik.98@outlook.com>
2022-01-14 13:13:28 +0100

    fix MPI

--- MPI1/MPI1.cpp
+++ MPI1/MPI1.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
     float tab[MAX];
     int k = 3;
     int my_rank;
-    int size = 10;
+    int size = 2;
     int i, d;
     int y, z;
     int wartosc;
@@ -50,27 +50,37 @@ int main(int argc, char *argv[])
     double tmp = 0;
     double wynik = 0;
     double startParallel = 0.0, stopParallel = 0.0;
+    wartosc = MAX / size;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Request reql;
     MPI_Status status;
-    int* tabAll = new int{ size };
+    //int* tabAll = new int{ size };
+    y = wartosc * my_rank;
+    z = wartosc * my_rank + wartosc;
+    for (i = y; i < z; i++) {
+        tab[i] = i + 1;
+    }
+    for (int i = y; i < z; i++)
+        nums.push_back(rand() % 100000);
     if (my_rank == 0) {
         startParallel = MPI_Wtime();
+        KthLargest2(nums, k);
         for (d = 0; d < size; d++) {
             if (d != my_rank) {
                 int a = 4;
                 MPI_Send(&a, 1, MPI_INT, d, 13, MPI_COMM_WORLD);
             }
         }
+        stopParallel = MPI_Wtime();
     }
     else if (my_rank != 0) {
         int b;
         MPI_Recv(&b, 1, MPI_INT, 0, 13, MPI_COMM_WORLD, &status);
     }
-    reszta = MAX % size;
-    if (reszta == 0) wartosc = MAX / size;
+   // reszta = MAX % size;
+   /* if (reszta == 0) wartosc = MAX / size;
     else {
         if (my_rank == 0) wartosc = MAX;
         else wartosc = 0;
@@ -78,18 +88,10 @@ int main(int argc, char *argv[])
             cout << "Nie można podzielić tablicy na %d " << size << "równych części!";
             cout << "K-ta wartość zostanie wyliczona przez 1 proces.";
         }
-    }
-    y = wartosc * my_rank;
-    z = wartosc * my_rank + wartosc;
-    for (i = y; i < z; i++) {
-        tab[i] = i+1;
-    }
-    for (int i = y; i < z; i++)
-         nums.push_back(rand() % 100000);
+    } */
     
-    stopParallel = MPI_Wtime();
     cout << "Array: ";
-    double Diff = stopParallel - startParallel;
+    double Diff = (stopParallel - startParallel);
     // for (int i : nums)
       //   cout << i << "\n";
 
